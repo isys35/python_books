4a33c300e87786d2fab1b4579c7da4f7c89d0d4b
isys35 <isys35@mail.ru>
2021-03-09 17:01:59 +0300

    Глава 34. Завершено.

---
+++ book_1/34/withas.py
@@ -0,0 +1,22 @@
+class TraceBlock:
+    def message(self, arg):
+        print('running ' + arg)
+
+    def __enter__(self):
+        print('starting with block')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            print('exited normally\n')
+        else:
+            print('raise an exception! ' + str(exc_type))
+
+
+if __name__ == '__main__':
+    with TraceBlock() as action:
+        action.message('test 1')
+    with TraceBlock() as action:
+        action.message('test 2')
+        raise TypeError
+        print('not reached')
