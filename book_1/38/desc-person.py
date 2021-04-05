15457a7dbc4c57281fa6436cc0eec9263b3078e8
isys35 <isys35@mail.ru>
2021-04-05 12:08:50 +0300

    Глава 38. В процессе

---
+++ book_1/38/desc-person.py
@@ -0,0 +1,28 @@
+class Name:
+    "name descriptor docs"
+
+    def __get__(self, instance, owner):
+        print('fetch...')
+        return instance._name
+
+    def __set__(self, instance, value):
+        print('chnage...')
+        instance._name = value
+
+    def __delete__(self, instance):
+        print('remove...')
+        del instance._name
+
+
+class Person:
+    def __init__(self, name):
+        self._name = name
+
+    name = Name()
+
+
+bob = Person('Bob Smith')
+print(bob.name)
+bob.name = 'Robert Smith'
+print(bob.name)
+del bob.name
---
+++ book_1/38/prop-person-deco.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        "name property docs"
+        print('fetch...')
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        print('change...')
+        self._name = value
+
+    @name.deleter
+    def name(self):
+        print('remove...')
+        del self._name
+
+
+bob = Person('Bob Smith')
+print(bob.name)
+bob.name = 'Victor'
+
+print(Person.name.__doc__)
